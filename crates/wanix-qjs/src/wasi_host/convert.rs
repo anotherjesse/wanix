5ee62554252efe5252bc2711d95de2911a660d57
Jesse Andrews <anotherjesse@gmail.com>
2026-06-05 17:07:22 -0700

    Split qjs errno conversion

--- crates/wanix-qjs/src/wasi_host/convert.rs
+++ crates/wanix-qjs/src/wasi_host/convert.rs
@@ -5,21 +5,27 @@ use rust_wasi_quickjs::{
 use wanix_fs::FileType;
 use wanix_wasi::{Errno, FileStat, WasiFdStat, WasiFileType, WasiWhence};
 
+macro_rules! wanix_errno_to_quickjs {
+    ($errno:expr) => {
+        match $errno {
+            Errno::Success => QuickJsWasiErrno::Io,
+            Errno::Badf => QuickJsWasiErrno::Badf,
+            Errno::Inval => QuickJsWasiErrno::Inval,
+            Errno::Nametoolong => QuickJsWasiErrno::Nametoolong,
+            Errno::Noent => QuickJsWasiErrno::Noent,
+            Errno::Exist => QuickJsWasiErrno::Exist,
+            Errno::Notdir => QuickJsWasiErrno::Notdir,
+            Errno::Isdir => QuickJsWasiErrno::Isdir,
+            Errno::Notempty => QuickJsWasiErrno::Notempty,
+            Errno::Nosys => QuickJsWasiErrno::Nosys,
+            Errno::Notcapable => QuickJsWasiErrno::Notcapable,
+            Errno::Io => QuickJsWasiErrno::Io,
+        }
+    };
+}
+
 pub(super) fn convert_errno(errno: Errno) -> QuickJsWasiErrno {
-    match errno {
-        Errno::Success => QuickJsWasiErrno::Io,
-        Errno::Badf => QuickJsWasiErrno::Badf,
-        Errno::Inval => QuickJsWasiErrno::Inval,
-        Errno::Nametoolong => QuickJsWasiErrno::Nametoolong,
-        Errno::Noent => QuickJsWasiErrno::Noent,
-        Errno::Exist => QuickJsWasiErrno::Exist,
-        Errno::Notdir => QuickJsWasiErrno::Notdir,
-        Errno::Isdir => QuickJsWasiErrno::Isdir,
-        Errno::Notempty => QuickJsWasiErrno::Notempty,
-        Errno::Nosys => QuickJsWasiErrno::Nosys,
-        Errno::Notcapable => QuickJsWasiErrno::Notcapable,
-        Errno::Io => QuickJsWasiErrno::Io,
-    }
+    wanix_errno_to_quickjs!(errno)
 }
 
 fn convert_file_type(file_type: WasiFileType) -> QuickJsWasiFileType {
@@ -64,5 +70,39 @@ pub(super) fn convert_whence(whence: QuickJsWasiWhence) -> WasiWhence {
         QuickJsWasiWhence::Set => WasiWhence::Set,
         QuickJsWasiWhence::Cur => WasiWhence::Cur,
         QuickJsWasiWhence::End => WasiWhence::End,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rust_wasi_quickjs::QuickJsWasiErrno;
+    use wanix_wasi::Errno;
+
+    use super::convert_errno;
+
+    #[test]
+    fn errno_mapping_projects_each_wanix_errno_to_quickjs_errno() {
+        let cases = [
+            (Errno::Success, QuickJsWasiErrno::Io),
+            (Errno::Badf, QuickJsWasiErrno::Badf),
+            (Errno::Inval, QuickJsWasiErrno::Inval),
+            (Errno::Nametoolong, QuickJsWasiErrno::Nametoolong),
+            (Errno::Noent, QuickJsWasiErrno::Noent),
+            (Errno::Exist, QuickJsWasiErrno::Exist),
+            (Errno::Notdir, QuickJsWasiErrno::Notdir),
+            (Errno::Isdir, QuickJsWasiErrno::Isdir),
+            (Errno::Notempty, QuickJsWasiErrno::Notempty),
+            (Errno::Nosys, QuickJsWasiErrno::Nosys),
+            (Errno::Notcapable, QuickJsWasiErrno::Notcapable),
+            (Errno::Io, QuickJsWasiErrno::Io),
+        ];
+        for (wanix, quickjs) in cases {
+            assert_eq!(convert_errno(wanix), quickjs, "{wanix:?}");
+        }
+    }
+
+    #[test]
+    fn success_errno_maps_to_io_because_errors_should_not_be_successful() {
+        assert_eq!(convert_errno(Errno::Success), QuickJsWasiErrno::Io);
     }
 }
